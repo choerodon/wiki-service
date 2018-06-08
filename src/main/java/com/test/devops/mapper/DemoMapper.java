a3e0a8e5dac280b1b9ffa3831d8c02b999adf34b
crockitwood <crockitwood@gmail.com>
2018-06-08 23:27:12 +0800

    [ADD] init
    Signed-off-by: crockitwood <crockitwood@gmail.com>

---
+++ src/main/java/com/test/devops/mapper/DemoMapper.java
@@ -0,0 +1,10 @@
+package com.test.devops.mapper;
+
+import com.test.devops.entity.Demo;
+import io.choerodon.mybatis.common.BaseMapper;
+
+/**
+ * @author crcokitwood
+ */
+public interface DemoMapper extends  BaseMapper<Demo> {
+}
---
+++ src/main/java/com/test/devops/service/DemoService.java
@@ -0,0 +1,9 @@
+package com.test.devops.service;
+
+import com.test.devops.entity.Demo;
+
+import java.util.List;
+
+public interface DemoService {
+    List<Demo> queryAll();
+}
