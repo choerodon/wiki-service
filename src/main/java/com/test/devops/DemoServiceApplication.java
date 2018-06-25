e76380b3a91e185fea5cce386538056ae9d4d5ce
10894 <shengli.yuan@hand-china.com>
2018-06-25 20:15:20 +0800

    [ADD] 测试使用wiki-service访问XWiki的Rest Api接口

--- src/main/java/com/test/devops/DemoServiceApplication.java
+++ src/main/java/com/test/devops/DemoServiceApplication.java
@@ -1,17 +1,13 @@
 package com.test.devops;
 
-import io.choerodon.resource.annoation.EnableChoerodonResourceServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 
 @SpringBootApplication
-@EnableFeignClients
+/*@EnableFeignClients
 @EnableEurekaClient
-@EnableChoerodonResourceServer
+@EnableChoerodonResourceServer*/
 public class DemoServiceApplication {
 
     public static void main(String[] args){
--- src/main/java/com/test/devops/controller/DemoController.java
+++ src/main/java/com/test/devops/controller/DemoController.java
@@ -1,41 +1,41 @@
 package com.test.devops.controller;
 
-import com.test.devops.entity.Demo;
-import com.test.devops.service.DemoService;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
+import java.util.HashMap;
+
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import io.choerodon.core.iam.ResourceLevel;
-import io.choerodon.swagger.annotation.Permission;
-
-import java.util.List;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/v1")
 public class DemoController {
 
-    @Autowired
-    DemoService demoService;
 
-    @Permission(permissionPublic = true,level = ResourceLevel.USER)
-    @ApiOperation(value = "运行的Demo")
-    @RequestMapping(value = "/demos", method = RequestMethod.GET)
-    public List<Demo> getVersion() {
-        return demoService.queryAll();
-    }
+    private static String PAGE_GET_URl = "http://xwiki.saas.hand-china.com/rest/wikis/xwiki/spaces/TestApp/spaces/yslPage/pages/WebHome?objects=true";
 
-    @Permission(permissionPublic = true,level = ResourceLevel.USER)
-    @ApiOperation(value = "运行的Demo")
-    @RequestMapping(value = "/demos", method = RequestMethod.POST)
-    public void insertVersion(
-            @ApiParam("demo")
-            @RequestBody Demo demo
-    ) {
-        demoService.add(demo);
+    private RestTemplate restTemplate = new RestTemplate();
+
+
+    @GetMapping("/wiki")
+    public ResponseEntity<String> hello() {
+
+        HttpHeaders requestHeaders = new HttpHeaders();
+        String key = "Authorization";
+        String value = "Basic YWRtaW46aGFuZGhhbmQ=";
+        requestHeaders.add(key, value);
+
+        HttpEntity<String> requestEntity = new HttpEntity<String>(null, requestHeaders);
+
+        ResponseEntity<String> responseEntity = restTemplate.exchange(PAGE_GET_URl, HttpMethod.GET, requestEntity, String.class, new HashMap());
+        String body = responseEntity.getBody();
+        HttpStatus statusCode = responseEntity.getStatusCode();
+        if (statusCode.is2xxSuccessful()) {
+            System.out.println(body);
+        } else {
+            System.out.println(statusCode);
+        }
+        return new ResponseEntity<String>(body, HttpStatus.OK);
     }
 }
