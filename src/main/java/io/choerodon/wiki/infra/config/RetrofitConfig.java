78bce12b50c7ce2a5bd163a22b76b113257e0e06
Zenger <qinsheng.wang@hand-china.com>
2018-07-11 11:43:54 +0800

    [IMP] 更新权限认证

--- src/main/java/io/choerodon/wiki/infra/config/RetrofitConfig.java
+++ src/main/java/io/choerodon/wiki/infra/config/RetrofitConfig.java
@@ -3,6 +3,8 @@ package io.choerodon.wiki.infra.config;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,8 +19,13 @@ import io.choerodon.wiki.infra.feign.WikiClient;
 @Configuration
 public class RetrofitConfig {
 
+    private static final Logger logger = LoggerFactory.getLogger(RetrofitConfig.class);
+
     @Value("${wiki.url}")
     private String wikiUrl;
+
+    @Value("${wiki.token}")
+    private String wikiToken;
 
     /**
      * Retrofit 设置
@@ -30,9 +37,11 @@ public class RetrofitConfig {
         OkHttpClient.Builder okHttpClientBuilder = new OkHttpClient.Builder();
         okHttpClientBuilder.interceptors().add((Interceptor.Chain chain) -> {
             Request original = chain.request();
-//            CustomUserDetails customUserDetails = DetailsHelper.getUserDetails();
+            CustomUserDetails customUserDetails = DetailsHelper.getUserDetails();
+            logger.info("username:  " + customUserDetails.getUsername());
             Request.Builder requestBuilder = original.newBuilder()
-                    .header("Authorization", "Basic YWRtaW46aGFuZGhhbmQ=");
+                    .header("username", customUserDetails.getUsername())
+                    .header("wikitoken", wikiToken);
 
             Request request = requestBuilder.build();
             return chain.proceed(request);
